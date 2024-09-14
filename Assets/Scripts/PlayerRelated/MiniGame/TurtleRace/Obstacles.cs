51bd5fc27dad3f58605a01840b25c13e153346fc
Peter Erbeni <86953639+ProggersValentino@users.noreply.github.com>
2024-09-14 12:25:26 +1000

    yes

--- Assets/Scripts/PlayerRelated/MiniGame/TurtleRace/Obstacles.cs
+++ Assets/Scripts/PlayerRelated/MiniGame/TurtleRace/Obstacles.cs
@@ -16,12 +16,7 @@ public class Obstacles : MonoBehaviour
     // {
     //     Debug.LogWarning("yes");
     // }
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    
 
     // Update is called once per frame
     void Update()
